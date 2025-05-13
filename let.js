93d3311c514c04c4ae15a66688e268ddc8e5978d
ManishMandrai <manishkumarmandrai@gmail.com>
2025-05-14 04:39:01 +0530

    first commit

---
+++ let.js
@@ -0,0 +1,24 @@
+// var a = 5;
+// var a = 6
+
+// // a = 7;
+
+// {
+//     console.log(a);
+// }
+
+// const Learn = () => {
+//     var b = 10;
+//     console.log(b);
+// }
+// Learn()
+
+
+let a = 5
+a = 6
+
+{
+    let abc = 8;
+    console.log(abc);
+}
+    console.log(abc);
---
+++ time.js
@@ -0,0 +1,23 @@
+let a = 5;
+let b = 5.5;
+
+let c = "Manish"
+let D = "Manish"
+
+let E;
+
+let G = 12345678987654345678n
+
+symbol();
+
+const oj1 = {
+    RollNo : 123,
+    Name : "Manish",
+
+}
+
+oj1.Name;
+
+const arr = [1, 2, 3, "Roshani", 5, "Manish", 7]
+
+arr[1]
